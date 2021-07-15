d01a56d2d3067ca11e16490e126cea5eac1b6068
Alex Ladda <alex.ladda@gmail.com>
2021-07-15 10:14:16 +0200

    refactored, cleaned up and commented

--- anagram.py
+++ anagram.py
@@ -7,6 +7,7 @@ import boto3
 def is_anagram(a, b):
     """
     Returns True if both arguments are anagrams.
+    Returns False if strings are not anagrams or arguments are not strings.
     """
     try:
         if sorted(a) == sorted(b):
@@ -17,41 +18,83 @@ def is_anagram(a, b):
         print("An error has occured: ", repr(error))
 
 
-def parse_csv(file):
+def test_pairs(potential_anagrams):
     """
-    Reads csv file and returns a list of one list per row.
-    """
-    with open(file) as csvfile:
-        contents = csv.reader(csvfile, dialect='unix')
-        potential_anagrams = []
-        for row in contents:
-            potential_anagrams.append(row)
-        return potential_anagrams
-
-
-def test_csv(file):
-    """
-    Tests a given csv file for anagrams.
-    Returns a list of boolean values.
+    Expects a list with pairs of potential anagrams.
+    Tests those pairs for anagrams.
+    Returns a list of boolean values for each pair.
     """
     anagram_evaluation = []
-    for line in parse_csv(file):
-        anagram_evaluation.append(is_anagram(line[0], line[1]))
+    print(potential_anagrams)
+    for pair in potential_anagrams:
+        anagram_evaluation.append(is_anagram(pair[0], pair[1]))
     return anagram_evaluation
 
 
+def parse_csv_file(file):
+    """
+    Opens a local csv file.
+    Returns a list of lists: One item per line, each item with the
+    potential anagram pair.
+
+    Intended to be used locally from console.
+
+    Out of scope:
+    - validating file
+    """
+    potential_anagrams = []
+    with open(file) as csvfile:
+        contents = csv.reader(csvfile, dialect='unix')
+        for item in contents:
+            potential_anagrams.append(item)
+        return potential_anagrams
+
+
+def parse_csv_string(csv_body):
+    """
+    Expects the contents of a csv file in one single string.
+    Returns a list of lists: One item per line, each item with the
+    potential anagram pair.
+
+    Out of scope:
+    - validating list
+    """
+    potential_anagrams = []
+    print("csv_body", csv_body)
+    for row in csv_body:
+        potential_anagrams.append(row)
+    return potential_anagrams
+
+
 def lambda_handler(event, context):
+    """
+    Triggered on upload of any csv file to the specified bucket.
+    Returns a list of boolean values.
+    Intended to be used locally from console.
+    """
+
+    # logging to console
     print("Received event: " + json.dumps(event, indent=2))
+
+    # initializing s3 resource
     s3 = boto3.resource('s3')
+
+    # retrieving information from event
     bucket = event['Records'][0]['s3']['bucket']['name']
-    filename = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'],
-                                         encoding='utf-8')
+    filename = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key'],
+        encoding='utf-8')
+
+    # processing uploaded file
     try:
         csvfile = s3.Object(bucket, filename)
-        potential_anagrams = csvfile.get()['Body'].read().decode('utf-8')
-        print("contents of file: ", potential_anagrams)
-        for line in potential_anagrams.splitlines():
+        csv_body = csvfile.get()['Body'].read().decode('utf-8')
+        # logging raw contents to console
+        print("contents of file: ", csv_body)
+        # Logging evaluation results to console.
+        for line in csv_body.splitlines():
             print(is_anagram(line.split(",")[0], line.split(",")[1]))
+
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}.'.format(filename, bucket))
